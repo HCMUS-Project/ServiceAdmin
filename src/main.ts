98f6d52211454d1d2fe14b8fb6ac402270d5b65f
VuKhoiGVM <nguyenvukhoi150402@gmail.com>
2024-07-29 17:11:25 +0700

    fix: fix max legnth grpc

--- src/main.ts
+++ src/main.ts
@@ -8,16 +8,13 @@ async function bootstrap() {
     // Create a ConfigService instance
     const configService = new ConfigService();
     const port = configService.get<number>('PORT');
-    const host = configService.get<string>('ADMIN_HOST')
+    const host = configService.get<string>('ADMIN_HOST');
     // Create a microservice instance
     const app = await NestFactory.createMicroservice<MicroserviceOptions>(AppModule, {
         transport: Transport.GRPC,
         bufferLogs: true,
         options: {
-            package: [ 
-                'userToken', 
-                'managetenant',
-            ],
+            package: ['userToken', 'managetenant'],
             protoPath: join(__dirname, '../src/proto/main.proto'),
             url: `${host}:${port}`,
             loader: {
@@ -26,6 +23,8 @@ async function bootstrap() {
                 arrays: true,
                 includeDirs: [join(__dirname, '../src/proto/')],
             },
+            maxReceiveMessageLength: 1024 * 1024 * 100,
+            maxSendMessageLength: 1024 * 1024 * 100,
         },
     });
 
